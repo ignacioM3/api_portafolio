477ca9b75b5d3e4ae09ebc7fba06697e1167633b
ignacioM3 <ignacio.marquez159@gmail.com>
2022-06-10 23:15:54 -0300

    cambio .env

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -10,7 +10,7 @@ const Portafolio = () => {
     useEffect(() => {
         const fetchProyect = async () => {
             setLoad(true)
-            const url = import.meta.env.VITE_API
+            const url = import.meta.env.VITE_API_URL
             try {
                 const respuesta = await fetch(url)
                 const resultado = await respuesta.json()
