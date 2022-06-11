4612f985d46e32a92b45ac279ff8c7f4cc0b5a40
ignacioM3 <ignacio.marquez159@gmail.com>
2022-06-10 23:30:16 -0300

    sube variable de entorno

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -10,7 +10,7 @@ const Portafolio = () => {
     useEffect(() => {
         const fetchProyect = async () => {
             setLoad(!load)
-            const url = "https://my-json-server.typicode.com/ignacioM3/api_portafolio/proyectos"
+            const url = import.meta.env.VITE_API_URL
             try {
                 const respuesta = await fetch(url)
                 const resultado = await respuesta.json()
